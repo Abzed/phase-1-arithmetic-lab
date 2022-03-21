5fe1cb6b66a51982445f11ffc568858c97ac8583
Abzed <ibraabzed@gmail.com>
2022-03-21 15:23:13 +0300

    commit tree 38ae750b0b87e11d4bdb98e1255e701f09339835

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+const num1 = 31;
+const num2 = 2;
 
-// Write your code here
+const multiply = num1 * num2;
+
+const random = parseInt(Math.random() * 10) + 1;
+
+const num3  = 19;
+const num4 = 5;
+
+const mod = num3 % num4;
+
+const max = Math.max(3, 7, 8, 19, 4, 20)
+
+
+
+
+
+
+
+
+
